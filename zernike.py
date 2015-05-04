1322a4bd9b9ea6cbb469ed3020adc368fe75c384
Sergio Pascual <sergiopr@fis.ucm.es>
2015-05-04 23:10:25 +0200

    Fix wrong calls in eval

--- zernike.py
+++ zernike.py
@@ -6,41 +6,11 @@
 from __future__ import division
 
 import math
-from scipy.special import jacobi, eval_jacobi
 import numpy
-
-def zernike_calc(n, m, rho, phi):
-
-    if m < 0:
-        return zernike_odd_calc(n, -m, rho, phi)
-    else:
-        return zernike_even_calc(n, m, rho, phi)
-
-def zernike_odd_calc(n, m, rho, phi):
-    return zernike_R_calc(n, m, rho) * math.sin(m * phi)
-
-def zernike_even_calc(n, m, rho, phi):
-    return zernike_R_calc(n, m, rho) * math.cos(m * phi)
-
-def zernike_R_calc(m, n, rho):
-    # n>=m>=0
-
-    k = n - m
-
-    if k % 2 == 1:
-        return 0.0
-
-    n_jacobi = k // 2
-
-    comp = 1 - 2 * rho * rho
-    # jacobi form
-    jacobi_pol = jacobi(n=n_jacobi, alpha=m, beta=0)
-
-    print '|jpol', jacobi_pol, '1'
-    return (-1)**n_jacobi * rho**m * jacobi_pol(comp)
+from scipy.special import jacobi, eval_jacobi
 
 class ZernikePol(object):
-    def __init__(self, m, n):
+    def __init__(self, n, m):
         k = n - m
 
         if k % 2 == 1:
@@ -64,7 +34,7 @@ class ZernikePol(object):
         comp = 1 - 2 * rho * rho
         return self.sign * rho**self.m * self.jacobi_pol(comp)
 
-def eval_zernike_R(m, n, rho):
+def eval_zernike_R(n, m, rho):
     # n>=m
     # m>=0
 
@@ -77,12 +47,13 @@ def eval_zernike_R(m, n, rho):
 
     comp = 1 - 2 * rho * rho
     # jacobi form
+    print n_jacobi
     return (-1)**n_jacobi * rho**m * eval_jacobi(n_jacobi, m, 0, comp)
 
 def eval_zernike(n, m, rho, phi):
 
     if m < 0:
-        return eval_zernike_R(n, m, rho) * math.sin(m * phi)
+        return eval_zernike_R(n, -m, rho) * numpy.sin(-m * phi)
     else:
-        return eval_zernike_R(n, m, rho) * math.cos(m * phi)
+        return eval_zernike_R(n, m, rho) * numpy.cos(m * phi)
 
